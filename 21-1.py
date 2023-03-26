d2169bbabc70e5b63fa85136945b1526e3608b40
shevela-anton <anton-shevela@mail.ru>
2023-03-26 18:29:16 +0300

    Задание

---
+++ 21-1.py
@@ -0,0 +1,13 @@
+# Задание 21-1
+pupil_name - ФИО
+email - электронная почта
+phone - номер телефона
+birthday - день рождения
+payment - статус оплаты курса
+group_id - номер группы
+lesson_id - номер занятия
+lesson_topic - тема занятия
+lesson_pres - отметка о посещении занятия
+hometask_id - домашнее задание
+hometask_status - ДЗ выполнено/не выполнено
+diploma_mark - оценка за диплом
